ce4309d9c10f63fb9db1a03c45bd8c0df3ca5932
RobinNusantara <robinnusantara127@gmail.com>
2020-10-22 16:40:28 +0700

    change navigation state based on route location

--- src/components/bottom-navigation/bottom-navigation.component.jsx
+++ src/components/bottom-navigation/bottom-navigation.component.jsx
@@ -1,5 +1,5 @@
 import React, {Fragment, useState, useEffect} from 'react';
-import {Link} from 'react-router-dom';
+import {Link, useLocation} from 'react-router-dom';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import Typography from '@material-ui/core/Typography';
@@ -12,11 +12,29 @@ import useStyles from './bottom-navigation.styles';
 
 function NavigationBottom() {
   const classes = useStyles();
-  const [value, setValue] = useState('explore');
+  const [value, setValue] = useState(0);
+  const location = useLocation();
 
   useEffect(() => {
-
-  }, []);
+    const path = location.pathname;
+    switch (path) {
+      case '/':
+        setValue(0);
+        break;
+      case '/favorites':
+        setValue(1);
+        break;
+      case '/cart':
+        setValue(2);
+        break;
+      case '/signup':
+      case '/signin':
+        setValue(3);
+        break;
+      default:
+        break;
+    };
+  }, [location]);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -30,25 +48,25 @@ function NavigationBottom() {
         onChange={handleChange}>
         <BottomNavigationAction
           label={<Typography variant="subtitle2">EXPLORE</Typography>}
-          value="explore"
+          value={0}
           icon={<Icon className={classes.icon} icon={bxCompass}/>}
           to="/collections"
           component={Link}/>
         <BottomNavigationAction
           label={<Typography variant="subtitle2">FAVORITES</Typography>}
-          value="favorites"
+          value={1}
           icon={<Icon className={classes.icon} icon={outlineFavoriteBorder}/>}
           to="/favorites"
           component={Link}/>
         <BottomNavigationAction
           label={<Typography variant="subtitle2">CART</Typography>}
-          value="cart"
+          value={2}
           icon={<Icon className={classes.icon} icon={outlineShoppingBag}/>}
           to="/cart"
           component={Link}/>
         <BottomNavigationAction
           label={<Typography variant="subtitle2">ACCOUNT</Typography>}
-          value="user"
+          value={3}
           icon={<Icon className={classes.icon} icon={bxUser}/>}
           to="/signin"
           component={Link}/>
