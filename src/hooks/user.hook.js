b5a89bc510c54c0a9ea053e5d95c43258cc65837
RobinNusantara <robinnusantara127@gmail.com>
2020-11-23 00:06:33 +0700

    created custom hooks for updating data

--- src/hooks/user.hook.js
+++ src/hooks/user.hook.js
@@ -17,13 +17,33 @@ export const usePostData = ({uid, collection, payload}) => {
   return [res, callback];
 };
 
+export const useUpdateData = ({uid, collection, payload}) => {
+  const [res, setRes] = useState({data: null, error: null, isLoading: false});
+  const firestore = useFirestore();
+
+  const callback = useCallback((id) => {
+    firestore.collection('users')
+        .doc(uid)
+        .collection(collection)
+        .doc(id)
+        .update(payload)
+        .then((res) => setRes({data: res, isLoading: false, error: null}))
+        .catch((error) => setRes(({data: null, isLoading: false, error: error})));
+  }, [firestore, uid, collection, payload]);
+  return [res, callback];
+};
+
 export const useRemoveData = ({uid, collection}) => {
   const [res, setRes] = useState({data: null, error: null, isLoading: false});
   const firestore = useFirestore();
 
   const callback = useCallback((id) => {
     setRes((prevState) => ({...prevState, isLoading: true}));
-    firestore.delete(`users/${uid}/${collection}/${id}`)
+    firestore.collection('users')
+        .doc(uid)
+        .collection(collection)
+        .doc(id)
+        .delete()
         .then((res) => setRes({data: res, isLoading: false, error: null}))
         .catch((error) => setRes(({data: null, isLoading: false, error: error})));
   }, [firestore, uid, collection]);
