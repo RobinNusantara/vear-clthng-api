dfc640c202193b79d0bf338f5ce4b2a51b149bfe
RobinNusantara <robinnusantara127@gmail.com>
2020-12-15 18:08:24 +0700

    created post product action

--- src/actions/products.action.js
+++ src/actions/products.action.js
@@ -21,12 +21,52 @@ function fetchProductFailed(error) {
   };
 }
 
+function insertProductStart() {
+  return {
+    type: ProductActionTypes.INSERT_PRODUCT_START,
+  };
+}
+
+function insertProductSuccess(product) {
+  return {
+    type: ProductActionTypes.INSERT_PRODUCT_SUCCESS,
+    payload: product,
+  };
+}
+
+function insertProductFailed(error) {
+  return {
+    type: ProductActionTypes.INSERT_PRODUCT_FAILED,
+    payload: error,
+  };
+}
+
+export function insertProduct(values) {
+  return (dispatch) => {
+    const formData = new FormData();
+    const files = values.files;
+    const headers = {headers: {'Content-Type': 'application/'}};
+
+    formData.append('productName', values.productName);
+    formData.append('productLabel', values.productLabel);
+    formData.append('productColor', values.productColor);
+    formData.append('productSize', values.productSize);
+    formData.append('productPrice', values.productPrice);
+
+    files.forEach((file) => formData.append('productImage', file));
+    dispatch(insertProductStart());
+    API.post('products/insert', formData, headers)
+        .then((res) => dispatch(insertProductSuccess(res)))
+        .catch((error) => dispatch(insertProductFailed(error)));
+  };
+}
+
 export function fetchProducts() {
   return (dispatch) => {
     dispatch(fetchProductStart());
     API.get('products/all')
         .then((res) => res.data)
         .then((res) => dispatch(fetchProductSuccess(res.data)))
-        .catch((error) => dispatch(fetchProductFailed(error.messages)));
+        .catch((error) => dispatch(fetchProductFailed(error)));
   };
 }
