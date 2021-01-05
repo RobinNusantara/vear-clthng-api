4418c9a291c3851299cd3eccc38b965d85506138
RobinNusantara <robinnusantara127@gmail.com>
2021-01-05 21:04:22 +0700

    rearrange connected router import

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {ConnectedRouter} from 'connected-react-router';
 import {Provider} from 'react-redux';
 import {persistStore} from 'redux-persist';
 import {PersistGate} from 'redux-persist/integration/react';
@@ -7,7 +8,6 @@ import {MuiThemeProvider} from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import theme from './themes/themes';
 import App from './components/app/app.component';
-import {ConnectedRouter} from 'connected-react-router';
 import configureStore, {history} from './store/store';
 
 const store = configureStore();
