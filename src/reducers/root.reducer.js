37b4dc9731db14749af64b3bfecf7171d28912d6
RobinNusantara <robinnusantara127@gmail.com>
2021-01-05 20:55:39 +0700

    rename favorites reducer

--- src/reducers/root.reducer.js
+++ src/reducers/root.reducer.js
@@ -2,14 +2,14 @@ import {combineReducers} from 'redux';
 import {connectRouter} from 'connected-react-router';
 import authReducer from './auth.reducer';
 import productsReducer from './products.reducer';
-import wishlistReducer from './wishlist.reducer';
+import favoritesReducer from './favorites.reducer';
 import cartsReducer from './carts.reducer';
 
 const rootReducer = (history) => combineReducers({
   router: connectRouter(history),
   auth: authReducer,
   shop: productsReducer,
-  favorites: wishlistReducer,
+  favorites: favoritesReducer,
   bags: cartsReducer,
 });
 
