abe3c061f3bbef70b6ac9d94d71e375eb5c3afca
RobinNusantara <robinnusantara127@gmail.com>
2021-01-10 22:11:01 +0700

    stop serving static file in server

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-const path = require('path');
 const authRoutes = require('./routes/auth-routes');
 const userRoutes = require('./routes/user-routes');
 const profileRoutes = require('./routes/profile-routes');
@@ -26,13 +25,5 @@ app.use('/api/v1/profile', profileRoutes);
 app.use('/api/v1/products', productRoutes);
 app.use('/api/v1/wishlist', wishlistRoutes);
 app.use('/api/v1/carts', cartRoutes);
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
