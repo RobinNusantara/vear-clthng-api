9a1f0db12076a2ea116e24957511a15fa32fd5de
RobinNusantara <robinnusantara127@gmail.com>
2021-03-02 13:12:33 +0700

    created auth controller and routes

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import morgan from 'morgan';
 import cors from 'cors';
 import { config as dotenv } from 'dotenv';
 import errorMiddleware from './middlewares/error.middleware';
+import AuthRoutes from './routes/auth.routes';
 import ProductRoutes from './routes/product.routes';
 
 class App {
@@ -20,7 +21,7 @@ class App {
   protected initializeMiddlewares(): void {
     this.app.use(morgan('dev'));
     this.app.use(cors());
-    this.app.use(bodyParser.urlencoded({ extended: true }));
+    this.app.use(bodyParser.json());
   }
 
   protected initializeErrorHandling(): void {
@@ -28,6 +29,7 @@ class App {
   }
 
   protected initializeRoutes(): void {
+    this.app.use('/api/v2/auth', AuthRoutes);
     this.app.use('/api/v2/products', ProductRoutes);
     this.app.use((req: Request, res: Response) => {
       res.status(404).send('Page Not Found');
--- src/routes/auth-routes.js
+++
@@ -1,9 +0,0 @@
-const {Router} = require('express');
-const controller = require('../controllers/auth-controller');
-
-const router = new Router();
-
-router.post('/signup', controller.register);
-router.post('/signin', controller.login);
-
-module.exports = router;
