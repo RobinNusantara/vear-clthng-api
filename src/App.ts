b9a69b85890736b9e6d25a9e8b94a1bdeb88bed5
RobinNusantara <robinnusantara127@gmail.com>
2022-01-21 19:50:24 +0700

    [CHG] bind container module

--- src/App.ts
+++ src/App.ts
@@ -1,16 +1,23 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable unused-imports/no-unused-vars */
 
-// Import Dependencies
+// Dependencies
 import "reflect-metadata";
+import { PrismaClient } from "@prisma/client";
 import cors from "cors";
 import { Application, json, urlencoded } from "express";
 import { Server } from "http";
-import { Container } from "inversify";
+import { Container, ContainerModule } from "inversify";
 import { InversifyExpressServer } from "inversify-express-utils";
 import morgan from "morgan";
 
-// Import Applications
+// Controllers
+import "@apps/controllers/UserController";
+
+// Modules
+import { modules } from "@apps/modules/Module";
+
+// Middlewares
 import { CreateError } from "@apps/common/middlewares/CreateErrorMiddleware";
 
 export class App {
@@ -20,7 +27,9 @@ export class App {
     constructor(
         private readonly _container: Container,
         private readonly _port: number,
-    ) {}
+    ) {
+        this.containers();
+    }
 
     public start(): Server {
         const server = new InversifyExpressServer(this._container, null, {
@@ -34,6 +43,19 @@ export class App {
             .listen(this._port, this._host, () => {
                 console.log(`Server running on port ${this._port}`);
             });
+    }
+
+    private containers(): void {
+        const dependencies = new ContainerModule((bind) => {
+            const prisma = new PrismaClient();
+            bind<PrismaClient>("PrismaClient").toConstantValue(prisma);
+        });
+
+        const applications = new ContainerModule((bind) => {
+            modules.forEach((module) => bind(Object(module)).toSelf());
+        });
+
+        return this._container.load(dependencies, applications);
     }
 
     private middlewares(app: Application): void {
