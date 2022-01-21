4fb31fb00bfcacf50692aa1ae4f3a1059b911ea3
RobinNusantara <robinnusantara127@gmail.com>
2022-01-21 18:47:48 +0700

    [ADD] application config

---
+++ src/common/config/ApplicationConfig.ts
@@ -0,0 +1,13 @@
+import { config } from "dotenv";
+
+config();
+
+export const applicationConfig = {
+    server: {
+        host: String(process.env.HOST) || "localhost",
+        port: Number(process.env.PORT) || 5000,
+    },
+    token: {
+        signature: String(process.env.JWT_SIGNATURE) || "JWT_SIGNATURE",
+    },
+};
