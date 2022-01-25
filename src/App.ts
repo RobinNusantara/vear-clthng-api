5bffdeaf681cc6617a47cdd557811715975a65ec
RobinNusantara <robinnusantara127@gmail.com>
2022-01-25 19:56:03 +0700

    [CHG] import api v1

--- src/App.ts
+++ src/App.ts
@@ -11,30 +11,29 @@ import { Container, ContainerModule } from "inversify";
 import { InversifyExpressServer } from "inversify-express-utils";
 import morgan from "morgan";
 
-// Controllers
-import "@apps/controllers/AuthController";
-import "@apps/controllers/UserController";
+// Routes
+import "@apps/controllers/v1";
 
 // Modules
-import { modules } from "@apps/modules/Module";
+import { repositories } from "@apps/repositories/modules";
+import { services } from "@apps/services/modules";
 
 // Middlewares
 import { CreateError } from "@apps/common/middlewares/CreateErrorMiddleware";
 
 export class App {
-    private readonly _version: string = process.env.VERSION || "v1";
     private readonly _host: string = process.env.HOST || "localhost";
 
     constructor(
         private readonly _container: Container,
         private readonly _port: number,
     ) {
-        this.containers();
+        this.modules();
     }
 
     public start(): Server {
         const server = new InversifyExpressServer(this._container, null, {
-            rootPath: `/api/${this._version}`,
+            rootPath: "/api",
         });
 
         return server
@@ -46,17 +45,16 @@ export class App {
             });
     }
 
-    private containers(): void {
-        const dependencies = new ContainerModule((bind) => {
+    private modules(): void {
+        const containers = new ContainerModule((bind) => {
             const prisma = new PrismaClient();
             bind<PrismaClient>("PrismaClient").toConstantValue(prisma);
+
+            repositories(bind);
+            services(bind);
         });
 
-        const applications = new ContainerModule((bind) => {
-            modules.forEach((module) => bind(Object(module)).toSelf());
-        });
-
-        return this._container.load(dependencies, applications);
+        return this._container.load(containers);
     }
 
     private middlewares(app: Application): void {
