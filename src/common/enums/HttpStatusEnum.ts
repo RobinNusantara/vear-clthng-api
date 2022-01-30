cc5b5a9e7c486ee2464550670cfd7be2d2f1e925
RobinNusantara <robinnusantara127@gmail.com>
2022-01-30 17:55:53 +0700

    [ADD] forbidden and unauthorized status

--- src/common/enums/HttpStatusEnum.ts
+++ src/common/enums/HttpStatusEnum.ts
@@ -3,6 +3,8 @@ export enum HttpStatus {
     Created = 201,
     NoContent = 204,
     BadRequest = 400,
+    Unauthorized = 401,
+    Forbidden = 403,
     NotFound = 404,
     Conflict = 409,
     UnprocessableEntity = 422,
