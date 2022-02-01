1a49e83bf5fd8edff496c981439701b2b155f23a
RobinNusantara <robinnusantara127@gmail.com>
2022-02-01 17:41:35 +0700

    [FIX] auth dto

--- src/dtos/AuthDto.ts
+++ src/dtos/AuthDto.ts
@@ -5,7 +5,6 @@ import {
     MaxLength,
     MinLength,
 } from "class-validator";
-import { ApiModel, ApiModelProperty } from "swagger-express-ts";
 
 interface IAuthDto {
     email: string;
@@ -13,29 +12,16 @@ interface IAuthDto {
     password: string;
 }
 
-@ApiModel({ name: "SignUpDto" })
 export class SignUpDto implements IAuthDto {
-    @ApiModelProperty({
-        required: true,
-        type: "string",
-    })
     @IsNotEmpty()
     @IsString()
     @IsEmail()
     email: string;
 
-    @ApiModelProperty({
-        required: true,
-        type: "string",
-    })
     @IsNotEmpty()
     @IsString()
     username: string;
 
-    @ApiModelProperty({
-        required: true,
-        type: "string",
-    })
     @IsNotEmpty()
     @IsString()
     @MinLength(8)
@@ -43,21 +29,12 @@ export class SignUpDto implements IAuthDto {
     password: string;
 }
 
-@ApiModel({ name: "SignInDto" })
 export class SignInDto implements Omit<IAuthDto, "username"> {
-    @ApiModelProperty({
-        required: true,
-        type: "string",
-    })
     @IsNotEmpty()
     @IsString()
     @IsEmail()
     email: string;
 
-    @ApiModelProperty({
-        required: true,
-        type: "string",
-    })
     @IsNotEmpty()
     @IsString()
     password: string;
