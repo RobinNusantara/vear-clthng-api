6a4cb6c84329d85f336e8c6c0c72122e91c9f7df
RobinNusantara <robinnusantara127@gmail.com>
2022-02-01 18:56:55 +0700

    [ADD] crud routes for admin

--- src/controllers/v1/ProvincyController.ts
+++ src/controllers/v1/ProvincyController.ts
@@ -4,11 +4,19 @@ import {
     Authentication,
 } from "@apps/common/middlewares/AuthenticationMiddleware";
 import { ValidateData } from "@apps/common/middlewares/ValidateDataMiddleware";
-import { CreateProvincyDto } from "@apps/dtos/ProvincesDto";
+import { CreateProvincyDto, UpdateProvincyDto } from "@apps/dtos/ProvincyDto";
 import { SERVICE_TYPES } from "@apps/services/modules";
 import { ProvincyService } from "@apps/services/ProvincyService";
 import { inject } from "inversify";
-import { controller, httpPost, requestBody } from "inversify-express-utils";
+import {
+    controller,
+    httpDelete,
+    httpGet,
+    httpPost,
+    httpPut,
+    requestBody,
+    requestParam,
+} from "inversify-express-utils";
 import { JsonResult } from "inversify-express-utils/lib/results";
 
 @controller("/v1/provinces")
@@ -20,15 +28,57 @@ export class ProvincyController extends Controller {
         super();
     }
 
+    @httpGet("/:provincyId")
+    async getProvincy(
+        @requestParam("provincyId") provincyId: string,
+    ): Promise<JsonResult> {
+        const data = await this._provincyService.getProvincy(provincyId);
+
+        return this.response(data);
+    }
+
+    /**
+     * These routes only access by admin
+     */
+
     @httpPost(
         "/",
-        Authentication.verify({ roles: access["*"] }),
+        Authentication.verify({ roles: access["Admin"] }),
         ValidateData.requestBody(CreateProvincyDto),
     )
     async insertProvincy(
         @requestBody() body: CreateProvincyDto,
     ): Promise<JsonResult> {
         const data = await this._provincyService.insertProvincy(body);
+
+        return this.response(data);
+    }
+
+    @httpPut(
+        "/:provincyId",
+        Authentication.verify({ roles: access["Admin"] }),
+        ValidateData.requestBody(UpdateProvincyDto),
+    )
+    async updateProvincy(
+        @requestParam("provincyId") provincyId: string,
+        @requestBody() body: UpdateProvincyDto,
+    ): Promise<JsonResult> {
+        const data = await this._provincyService.updateProvincy(
+            provincyId,
+            body,
+        );
+
+        return this.response(data);
+    }
+
+    @httpDelete(
+        "/:provincyId",
+        Authentication.verify({ roles: access["Admin"] }),
+    )
+    async removeProvincy(
+        @requestParam("provincyId") provincyId: string,
+    ): Promise<JsonResult> {
+        const data = await this._provincyService.removeProvincy(provincyId);
 
         return this.response(data);
     }
