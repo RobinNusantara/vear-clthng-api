d7afd154c2212745519d8ae9d57cd8e394b77457
RobinNusantara <robinnusantara127@gmail.com>
2022-02-01 19:08:59 +0700

    [ADD] provincy validation

--- src/validations/modules/index.ts
+++ src/validations/modules/index.ts
@@ -1,15 +1,21 @@
 import { interfaces } from "inversify";
 import { IContainerInterface } from "@apps/common/interfaces/ContainerInterface";
 import { CountryValidation } from "@apps/validations/CountryValidation";
+import { ProvincyValidation } from "@apps/validations/ProvincyValidation";
 
 export const VALIDATION_TYPES = {
     CountryValidation: Symbol.for("CountryValidation"),
+    ProvincyValidation: Symbol.for("ProvincyValidation"),
 };
 
 const containers: Array<IContainerInterface> = [
     {
         type: VALIDATION_TYPES.CountryValidation,
         class: CountryValidation,
+    },
+    {
+        type: VALIDATION_TYPES.ProvincyValidation,
+        class: ProvincyValidation,
     },
 ];
 
