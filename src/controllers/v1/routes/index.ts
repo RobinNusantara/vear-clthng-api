4811dc4b6e7726ad8669bc15aa8bcdc7ed7904d9
RobinNusantara <robinnusantara127@gmail.com>
2022-02-10 23:04:05 +0700

    [ADD] city controller

--- src/controllers/v1/routes/index.ts
+++ src/controllers/v1/routes/index.ts
@@ -2,3 +2,4 @@ export * from "@apps/controllers/v1/AuthController";
 export * from "@apps/controllers/v1/UserController";
 export * from "@apps/controllers/v1/CountryController";
 export * from "@apps/controllers/v1/ProvincyController";
+export * from "@apps/controllers/v1/CityController";
