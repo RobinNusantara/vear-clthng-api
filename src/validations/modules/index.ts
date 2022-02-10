4c0ce7cc0dd4156c330dfdf710302d961390d408
RobinNusantara <robinnusantara127@gmail.com>
2022-02-10 23:01:52 +0700

    [ADD] bind validation container

--- src/validations/modules/index.ts
+++ src/validations/modules/index.ts
@@ -2,10 +2,12 @@ import { interfaces } from "inversify";
 import { IContainerInterface } from "@apps/common/interfaces/ContainerInterface";
 import { CountryValidation } from "@apps/validations/CountryValidation";
 import { ProvincyValidation } from "@apps/validations/ProvincyValidation";
+import { CityValidation } from "@apps/validations/CityValidation";
 
 export const VALIDATION_TYPES = {
     CountryValidation: Symbol.for("CountryValidation"),
     ProvincyValidation: Symbol.for("ProvincyValidation"),
+    CityValidation: Symbol.for("CityValidation"),
 };
 
 const containers: Array<IContainerInterface> = [
@@ -16,6 +18,10 @@ const containers: Array<IContainerInterface> = [
     {
         type: VALIDATION_TYPES.ProvincyValidation,
         class: ProvincyValidation,
+    },
+    {
+        type: VALIDATION_TYPES.CityValidation,
+        class: CityValidation,
     },
 ];
 
