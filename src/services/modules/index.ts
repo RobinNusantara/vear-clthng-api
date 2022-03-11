bc15e277b99b516fca9744934af20659b3a88100
RobinNusantara <robinnusantara127@gmail.com>
2022-03-12 01:23:25 +0700

    [ADD] product repositories and product services

--- src/services/modules/index.ts
+++ src/services/modules/index.ts
@@ -1,20 +1,29 @@
 import { interfaces } from "inversify";
 import { IContainerInterface } from "@apps/common/interfaces/ContainerInterface";
+// Users
 import { UserService } from "@apps/services/UserService";
 import { AuthService } from "@apps/services/AuthService";
+// Areas
 import { CountryService } from "@apps/services/CountryService";
 import { ProvincyService } from "@apps/services/ProvincyService";
 import { CityService } from "@apps/services/CityService";
+// Products
+import { ProductService } from "@apps/services/ProductService";
 
 export const SERVICE_TYPES = {
+    // Users
     AuthService: Symbol.for("AuthService"),
     UserService: Symbol.for("UserService"),
+    // Areas
     CountryService: Symbol.for("CountryService"),
     ProvincyService: Symbol.for("ProvincyService"),
     CityService: Symbol.for("CityService"),
+    // Products
+    ProductService: Symbol.for("ProductService"),
 };
 
 const containers: Array<IContainerInterface> = [
+    // Users
     {
         type: SERVICE_TYPES.AuthService,
         class: AuthService,
@@ -23,6 +32,7 @@ const containers: Array<IContainerInterface> = [
         type: SERVICE_TYPES.UserService,
         class: UserService,
     },
+    // Areas
     {
         type: SERVICE_TYPES.CountryService,
         class: CountryService,
@@ -34,6 +44,11 @@ const containers: Array<IContainerInterface> = [
     {
         type: SERVICE_TYPES.CityService,
         class: CityService,
+    },
+    // Products
+    {
+        type: SERVICE_TYPES.ProductService,
+        class: ProductService,
     },
 ];
 
