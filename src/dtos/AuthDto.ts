23822c72ed3201470880900dfcb84bc19fb46731
RobinNusantara <robinnusantara127@gmail.com>
2022-04-13 21:50:50 +0700

    [ADD] signup dto

---
+++ src/dtos/AuthDto.ts
@@ -0,0 +1,24 @@
+import {
+    IsString,
+    IsNotEmpty,
+    IsEmail,
+    MaxLength,
+    MinLength,
+} from "class-validator";
+
+export class SignUpDto {
+    @IsString()
+    @IsNotEmpty()
+    username: string;
+
+    @IsString()
+    @IsEmail()
+    @IsNotEmpty()
+    email: string;
+
+    @IsString()
+    @MinLength(8)
+    @MaxLength(16)
+    @IsNotEmpty()
+    password: string;
+}
