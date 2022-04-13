333ff61c54b8be1ec833ca3ab54a38c1c0c78a43
RobinNusantara <robinnusantara127@gmail.com>
2022-04-13 20:36:56 +0700

    [ADD] sequelize and database config

---
+++ sequelize/index.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+
+module.exports = {
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": "mysql"
+}
