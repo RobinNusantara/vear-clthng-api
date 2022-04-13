970b7f5859d7e42deb3285671c4922e0ed91cade
RobinNusantara <robinnusantara127@gmail.com>
2022-04-13 21:49:45 +0700

    [CHG] payload

--- src/common/interfaces/JwtPayloadInterface.ts
+++ src/common/interfaces/JwtPayloadInterface.ts
@@ -1,6 +1,9 @@
+import { Role } from "../enums/RoleEnum";
+
 export interface IJwtPayload {
-    id: string;
+    id: string | undefined;
     username: string;
     email: string;
-    role: string;
+    role: Role;
+    status: number;
 }
