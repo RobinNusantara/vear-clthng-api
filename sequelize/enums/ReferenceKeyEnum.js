1fae1cfb3e4fed61c002f2705b90b47cf8f7e847
RobinNusantara <robinnusantara127@gmail.com>
2022-04-14 14:27:22 +0700

    [ADD] enums for reference key

---
+++ sequelize/enums/ReferenceKeyEnum.js
@@ -0,0 +1,9 @@
+const ReferenceKey = Object.freeze({
+    CASCADE: "CASCADE",
+    RESTRICT: "RESTRICT",
+    SET_DEFAULT: "SET DEFAULT",
+    SET_NULL: "SET NULL",
+    NO_ACTION: "NO ACTION",
+});
+
+module.exports = { ReferenceKey };
