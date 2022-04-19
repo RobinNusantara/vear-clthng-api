d833161decfc8c3dc211ca9822d02af7a9a4d403
RobinNusantara <robinnusantara127@gmail.com>
2022-04-19 21:51:45 +0700

    [CHG] database to singleton

--- src/infrastructures/database/Database.ts
+++ src/infrastructures/database/Database.ts
@@ -2,11 +2,29 @@ import { Sequelize } from "sequelize-typescript";
 import { config } from "@apps/common/config/AppConfig";
 import { models } from "@apps/models";
 
-const { database } = config;
+class PrimeDatabase extends Sequelize {
+    private static _instance: PrimeDatabase;
 
-export const Database = new Sequelize({
-    dialect: "mysql",
-    ...database,
-    logging: false,
-    models,
-});
+    constructor() {
+        super({
+            dialect: "mysql",
+            host: config.database.host,
+            port: config.database.port,
+            database: "db_vear_clthng",
+            username: config.database.username,
+            password: config.database.password,
+            logging: false,
+            models,
+        });
+    }
+
+    public static getInstance(): PrimeDatabase {
+        if (!this._instance) {
+            this._instance = new PrimeDatabase();
+        }
+
+        return this._instance;
+    }
+}
+
+export const Database = PrimeDatabase.getInstance();
