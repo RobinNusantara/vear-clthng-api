9d14fb2c10887de871291fc8d3d15e5d0a8241dc
RobinNusantara <robinnusantara127@gmail.com>
2022-04-23 22:14:54 +0700

    [ADD] create product dto

--- src/dtos/ProductDto.ts
+++ src/dtos/ProductDto.ts
@@ -1,13 +1,50 @@
 import { CurrencyFormatter } from "@apps/common/formatter/CurrencyFormatter";
 import { ProductModel } from "@apps/models/ProductModel";
+import { Type } from "class-transformer";
+import {
+    IsArray,
+    IsDefined,
+    IsEmpty,
+    IsNotEmpty,
+    IsNumber,
+    IsString,
+    ValidateNested,
+} from "class-validator";
 import { VariantDto } from "./VariantDto";
+
+export class CreateProductVariantDto {
+    @IsNumber({}, { message: "Color Id must be a numeric value!" })
+    colorId: number;
+}
+
+export class CreateProductDto {
+    @IsString({ message: "Name must be a string!" })
+    @IsNotEmpty({ message: "Name should not be empty!" })
+    name: string;
+
+    @IsNumber({}, { message: "Brand Id must be a numeric value!" })
+    brandId: number;
+
+    @IsNumber({}, { message: "Price must be a numeric value!" })
+    price: number;
+
+    @IsString({ message: "Description must be a string!" })
+    @IsEmpty()
+    description: string;
+
+    @IsDefined()
+    @IsArray({ message: "Variants must be type of array!" })
+    @ValidateNested({ each: true })
+    @Type(() => CreateProductVariantDto)
+    variants: Array<CreateProductVariantDto>;
+}
 
 export class ProductDto {
     id: string;
 
     name: string;
 
-    brand: string;
+    brand: string | null;
 
     price: string;
 
@@ -19,7 +56,7 @@ export class ProductDto {
         return {
             id: model.getDataValue("id"),
             name: model.getDataValue("name"),
-            brand: model.brand.getDataValue("name"),
+            brand: model.brand ? model.brand.getDataValue("name") : null,
             // eslint-disable-next-line prettier/prettier
             price: CurrencyFormatter.formatToRupiah(model.getDataValue("price")),
             description: model.getDataValue("description"),
