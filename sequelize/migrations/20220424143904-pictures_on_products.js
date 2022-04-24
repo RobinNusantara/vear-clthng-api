911f4c7eea11497a70a5fd9504eff45aa07c06ae
RobinNusantara <robinnusantara127@gmail.com>
2022-04-25 01:44:35 +0700

    [ADD] pictures products migration

---
+++ sequelize/migrations/20220424143904-pictures_on_products.js
@@ -0,0 +1,48 @@
+'use strict';
+const { DataTypes } = require("sequelize");
+const { Timestamp } = require("../base/TimestampFactory");
+const { ReferenceKey } = require("../enums/ReferenceKeyEnum");
+
+/**
+ * @typedef {import("sequelize").QueryInterface} QueryInterface
+ * @typedef {import("sequelize").Sequelize} Sequelize
+ */
+
+module.exports = {
+    /**
+     * @param {QueryInterface} queryInterface 
+     * @param {Sequelize} Sequelize 
+     */
+    async up (queryInterface, Sequelize) {
+        return await queryInterface.createTable("pictures_on_products", {
+            idProductFk: {
+                field: "id_product_fk",
+                type: DataTypes.UUID,
+                allowNull: false,
+                references: {
+                    model: "products",
+                    key: "id"
+                },
+                onUpdate: ReferenceKey.CASCADE,
+                onDelete: ReferenceKey.CASCADE,
+            },
+            idPictureFk: {
+                field: "id_picture_fk",
+                type: DataTypes.UUID,
+                allowNull: true,
+                references: {
+                    model: "pictures",
+                    key: "id",
+                },
+                onUpdate: ReferenceKey.CASCADE,
+                onDelete: ReferenceKey.CASCADE,
+            },
+            ...Timestamp,
+        });
+    },
+    /**
+     * @param {QueryInterface} queryInterface 
+     * @param {Sequelize} Sequelize 
+     */
+    async down (queryInterface, Sequelize) {}
+};
