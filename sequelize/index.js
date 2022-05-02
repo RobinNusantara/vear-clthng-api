de501bb3da853693e7b073ce51bd1296bdd2743e
RobinNusantara <robinnusantara127@gmail.com>
2022-05-02 07:20:01 +0700

    [CHG] database to postgres

--- sequelize/index.js
+++ sequelize/index.js
@@ -5,5 +5,5 @@ module.exports = {
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
-    "dialect": "mysql"
+    "dialect": "postgres"
 }
