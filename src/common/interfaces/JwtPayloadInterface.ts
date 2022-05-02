12e98d8c3f3e9901acd6439bc5709bde79e246bc
RobinNusantara <robinnusantara127@gmail.com>
2022-05-03 04:55:38 +0700

    [CHG] user status

--- src/common/interfaces/JwtPayloadInterface.ts
+++ src/common/interfaces/JwtPayloadInterface.ts
@@ -1,11 +1,12 @@
 import { Role } from "@apps/common/enums/RoleEnum";
+import { Status } from "@apps/common/enums/StatusEnum";
 
 export interface IJwtPayload {
     id: string;
     username: string;
     email: string;
     role: Role;
-    status: number;
+    status: Status;
     iat?: number;
     exp?: number;
 }
