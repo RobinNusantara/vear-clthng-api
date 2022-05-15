529d530529872017824f14071631e748f754ad84
RobinNusantara <robinnusantara127@gmail.com>
2022-05-15 16:17:10 +0700

    [ADD] message multer error

--- src/middlewares/CreateErrorMiddleware.ts
+++ src/middlewares/CreateErrorMiddleware.ts
@@ -4,6 +4,7 @@
 // Import Dependencies
 import { NextFunction, Request, Response } from "express";
 import { HttpError } from "http-errors";
+import { MulterError } from "multer";
 
 // Import Applications
 import { HttpStatus } from "@apps/common/enums/HttpStatusEnum";
@@ -22,6 +23,11 @@ export class CreateError {
         if (err instanceof HttpError) {
             statusCode = err.statusCode;
             messages = err.message;
+        } else if (err instanceof MulterError) {
+            const multer = CreateError.mapMulterError(err);
+
+            statusCode = multer.status;
+            messages = multer.messages;
         } else {
             console.log("============Error============");
             console.log(err);
@@ -34,5 +40,34 @@ export class CreateError {
         const response = ResponseFactory.errorResponse(statusCode, messages);
 
         return res.status(statusCode).json(response);
+    }
+
+    private static mapMulterError(error: MulterError) {
+        let status: HttpStatus;
+        let messages: string;
+
+        switch (error.code) {
+            case "LIMIT_FILE_SIZE":
+                status = HttpStatus.PayloadTooLarge;
+                messages = "File too large!";
+                break;
+            case "LIMIT_UNEXPECTED_FILE":
+                status = HttpStatus.RangeNotSatisfiable;
+                messages = "File limit exceeded!";
+                break;
+            default:
+                console.log("======Upload File Error======");
+                console.log(error);
+                console.log("<<=========================>>");
+
+                status = HttpStatus.BadRequest;
+                messages = "Bad request";
+                break;
+        }
+
+        return {
+            status,
+            messages,
+        };
     }
 }
